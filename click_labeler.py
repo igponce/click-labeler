9a2dd07ac257d9a05c0ba019addae32931ffbebd
Iñigo González <igponce@gmail.com>
2017-09-05 11:47:20 +0200

    Images scaled to 400x400 px
    Big (megapixel) images can be grater than screen space, making impossible to label them.
    Easy fix: scale all to 400x400px.

--- click_labeler.py
+++ click_labeler.py
@@ -1,10 +1,14 @@
 # import the necessary packages
 import argparse
 import cv2
+import numpy as np
+from scipy.ndimage import zoom
+from skimage.transform import resize
 
 # initialize the list of reference points and state
 points = []
 state = 'click_loop'
+filename='foto'
 
 def click_loop (event, x, y, flags, param):
     # grab references to the global variables
@@ -13,15 +17,15 @@ def click_loop (event, x, y, flags, param):
     # Point = Left button UP
     if event == cv2.EVENT_LBUTTONUP:
         points.append((x, y))
-
+        
         # draw a point
-        cv2.circle(image, (x,y) , 5, (0,255,0), 3)
-        cv2.putText(image,str(len(points)),(x+y), 1, 2,(0,0,0),2,cv2.LINE_AA)
-        cv2.putText(image,str(len(points)),(x+1,y+1), 1, 2,(255,255,255),2,cv2.LINE_AA)
-        cv2.imshow("foto", image)
-        print("Point @: {} {}".format(x,y))
+        cv2.circle(image, (x,y) , 5, (0,255,0), 3) 
+        cv2.putText(image,str(len(points)),(x+1,y+1), 1, 2,(0,0,0),2,cv2.LINE_AA)
+        cv2.putText(image,str(len(points)),(x,y), 1, 2,(255,255,255),2,cv2.LINE_AA)
+        cv2.imshow(filename, image)
+        print("Point @: {} {}".format(x,y)) 
 
-
+ 
 # construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="Path to the image")
@@ -30,22 +34,27 @@ args = vars(ap.parse_args())
 # load the image, clone it, and setup the mouse callback function
 
 filename = args["image"]
-image = cv2.imread(filename)
+image = cv2.imread(filename) 
+# image = zoom( image, 300/image.shape[0], np.uint8 )
+image = resize( image, (400,400) )
 clone = image.copy()
-cv2.namedWindow("foto")
-cv2.setMouseCallback("foto", click_loop)
+
+cv2.namedWindow(filename)
+cv2.setMouseCallback(filename, click_loop)
 
 print("""Instructions:
 
   r   - RESET
   q   - QUIT without saving
-  s,x - (s)AVE, E(x)IT saving results to filename.json
-""")
+  s,x - (s)AVE, E(x)IT saving results to filename.points
+
+  File: {}
+""".format(filename) )
 
 # Loop until keyboard exit
 while True:
     # display the image and wait for a keypress
-    cv2.imshow("foto", image)
+    cv2.imshow(filename, image)
     key = cv2.waitKey(1) & 0xFF
 
     # if the 'r' key is pressed, reset the cropping region
@@ -78,6 +87,3 @@ if state == "SAVE" :
 
 # close all open windows
 cv2.destroyAllWindows()
-
-# EOF
-
